323293541ae9b6e621f8851797c28f0b0d89b2af
RobustBiscuit <irshaaddodia@gmail.com>
2019-08-07 22:13:27 +0200

    this is first iteration with setMUX and LED/PT select functions

--- LED_PT_Matrix_Plate_Reader.py
+++ LED_PT_Matrix_Plate_Reader.py
@@ -12,20 +12,38 @@ import RPi.GPIO as GPIO
 import time
 
 #configuration
-GPIO.setmode(BCM)   #Broadcom Pin Configuration
+GPIO.setmode(GPIO.BCM)   #Broadcom Pin Configuration
 
 
 #output pins for MUX selection
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(19,GPIO.OUT)
-GPIO.setup(26,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(20,GPIO.OUT)
-GPIO.setup(21,GPIO.OUT)
+#MUX 1 - controls ROWS, A,B,C
+GPIO.setup(13,GPIO.OUT) #a
+GPIO.setup(19,GPIO.OUT) #b
+GPIO.setup(26,GPIO.OUT) #c
+#MUX 2 - controlls COLS, I thru VI
+GPIO.setup(16,GPIO.OUT) #a
+GPIO.setup(20,GPIO.OUT) #b
+GPIO.setup(21,GPIO.OUT) #c
 
 ########################################################################################
 #FUNCTION DECLARATIONS
 ########################################################################################
+
+def setMUX(MUX, a, b, c):
+    """this function allows you to set the MUX selection by parsing either MUX 1 or 2 ,
+        and the corresponding a,b,c commands (HIGH or LOW). Irshaad fix this doc string it is terrible"""
+    
+    if MUX==1: #ROWS: A,B,C
+        GPIO.output(13, GPIO.a) #MUX 1a
+        GPIO.output(19, GPIO.b) #MUX 1b
+        GPIO.output(26, GPIO.c) #MUX 1c
+        
+        
+    if MUX==2: #COLS: I,II,III,IV,V,VI
+        GPIO.output(16, GPIO.a) #MUX 2a
+        GPIO.output(20, GPIO.b) #MUX 2b
+        GPIO.output(21, GPIO.c) #MUX 2c
+
 
 def choose(sensor):
     """This function allows you to choose an LED or PT (i.e. turning it ON) by parsing the
@@ -36,14 +54,43 @@ def choose(sensor):
     C 13  14  15  16   17   18
     
     currently this only supports a 3x6 matrix"""
+    
     A = ("0", "1", "2", "3", "4", "5")
     B = ("6", "7", "8", "9", "10", "11")
     C = ("12", "13", "14", "15", "16", "17")
     
-    I = ("","","")
-    II = ("","","")
-    III = ("","","")
-    IV = ("","","")
-    V = ("","","")
-    VI = ("","","")
+    I = ("0","6","13")
+    II = ("1","7","14")
+    III = ("2","9","15")
+    IV = ("3","10","16")
+    V = ("4","11","17")
+    VI = ("5","12","18")
+    
+    if sensor in A:
+        setMUX(1, LOW, LOW, LOW) #CH0
+        
+    elif sensor in B:
+        setMUX(1, LOW, LOW, HIGH) #CH1
+        
+    elif sensor in C:
+        setMUX(1, LOW, HIGH, LOW) #CH2
+        
+        
+    if sensor in I:
+        setMUX(2, LOW, LOW, LOW) #CH0
+        
+    elif sensor in II:
+        setMUX(2, LOW, LOW, HIGH) #CH1
+        
+    elif sensor in III:
+        setMUX(2, LOW, HIGH, LOW) #CH2
+        
+    elif sensor in IV:
+        setMUX(2, LOW, HIGH, HIGH) #CH3
+        
+    elif sensor in V:
+        setMUX(2, HIGH, LOW, LOW) #CH4
+        
+    elif sensor in VI:
+        setMUX(2, HIGH, LOW, HIGH) #CH5
     
